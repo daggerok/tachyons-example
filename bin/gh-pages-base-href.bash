0665010b26d5f96ce0fae42c7428c3c3cdf54737
Maksim Kostromin <daggerok@gmail.com>
2018-07-24 01:13:31 +0300

    Final commit, ready for tagging (C) whatthecommit.com

--- bin/gh-pages-base-href.bash
+++ bin/gh-pages-base-href.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # add base tag:
-# <head> -> <head><base href="/js-examples/"/>
+# <head> -> <head><base href="/tachyons-example/"/>
 
 if ! [ -z "$1" ]; then
   if [ -f "$1" ]; then
-    sed -i -e "s/\(<head>\)/<head><base href=\"\/js-examples\/\"\/>/g" $1
+    sed -i -e "s/\(<head>\)/<head><base href=\"\/tachyons-example\/\"\/>/g" $1
     rm -rf "$1-e"
   fi
 fi
